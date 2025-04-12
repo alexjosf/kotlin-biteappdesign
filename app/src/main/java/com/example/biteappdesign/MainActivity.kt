7e1b395e174306b65f9271bec83459b7e5947761
alexjosf <edavvealex@gmail.com>
2025-04-11 22:10:08 -0400

    Final

--- app/src/main/java/com/example/biteappdesign/MainActivity.kt
+++ app/src/main/java/com/example/biteappdesign/MainActivity.kt
@@ -179,8 +179,6 @@ fun Cards() {
                     }
                 }
         }
-
-
         LazyRow {
             items(imageTagMap.toList()) { (outerIndex, tags) ->
                 Column(
